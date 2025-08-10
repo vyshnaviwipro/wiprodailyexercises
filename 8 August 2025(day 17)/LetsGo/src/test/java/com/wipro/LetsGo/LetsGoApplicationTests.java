3cba5eec2149e401553b99eb27f7d1a10b6dc731
vyshnaviwipro <lakshmikathinokkula@gmail.com>
2025-08-11 00:03:34 +0530

    Add files via upload

---
+++ 8 August 2025(day 17)/LetsGo/src/test/java/com/wipro/LetsGo/LetsGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.LetsGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
