25fee78e8beeb4db5bdd21c93e40c8041b6da64c
vyshnaviwipro <lakshmikathinokkula@gmail.com>
2025-08-16 15:39:24 +0530

    DAY 23

---
+++ 16 August 2025 (day 23)/Employee/src/main/java/com/wipro/Employee/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.wipro.Employee.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntime(RuntimeException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
+
