cde7585e31163cea2ffaa65881e3528e8317613f
vyshnaviwipro <lakshmikathinokkula@gmail.com>
2025-08-30 08:41:13 +0530

    updated

---
+++ captsone_project/productms/src/main/java/com/wipro/productms/ProductmsApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.productms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductmsApplication.class, args);
+	}
+
+}
